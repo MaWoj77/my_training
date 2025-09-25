5f6fb88337379d37626cdb504dcb133711a63eec
MaWoj77 <wojewoda.m14@gmail.com>
2025-09-25 11:58:19 +0200

    update

--- set_methods.py
+++ set_methods.py
@@ -5,6 +5,10 @@ fr_s = set(input().split())
 
 new_set_union = en_s.union(fr_s)
 new_set_intersection = en_s.intersection(fr_s)
+new_set_difference = en_s.difference(fr_s)
+new_set_s_difference = en_s.symmetric_difference(fr_s)
 
 print(len(new_set_union))
 print(len(new_set_intersection))
+print(len(new_set_difference))
+print(len(new_set_s_difference))
